8d3324cd9a8957f9cde33be946de4267351e052d
Jose Ingunza <jose.ingunza.es@gmail.com>
2022-06-26 18:23:23 +0000

    nuevo avance

--- exercises/146.2-ArrayToObject/app.js
+++ exercises/146.2-ArrayToObject/app.js
@@ -1,6 +1,11 @@
 function fromListToObject(array) {
   // your code here
+  let obj = {};
+  for (let i in array){
+    obj[array[i][0]] = array[i][1];
+  }
+  return obj
 }
 
 let output = fromListToObject([['make', 'Ford'], ['model', 'Mustang'], ['year', 1964]])
-console.log(ouput) // => { make: 'Ford', model: 'Mustang', year: 1964 }
+console.log(output) // => { make: 'Ford', model: 'Mustang', year: 1964 }
--- exercises/146.3-ArrayToObject/app.js
+++ exercises/146.3-ArrayToObject/app.js
@@ -1,3 +1,13 @@
 function transformEmployeeData(array) {
   // your code here
+  const newArray = [];
+  for (let i=0; i<array.length; i++){
+    newArray[i]={};  // queda asi: [{},{}]
+    for (let j=0; j<array[i].length; j++){
+      for (let k=0; k<array[i][j].length; k++){
+        newArray[i][array[i][j][0]] = array[i][j][1];
+      }
+    }
+  }
+  return newArray;
 }
